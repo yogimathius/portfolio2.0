9f5a5f1694cd3960f4d90215867fb46f503a9099
Mathius <devachievem@gmail.com>
2021-04-29 08:35:36 -0700

    add portfolio images to assets

--- src/components/Navigation/NavigationLogo.jsx
+++ src/components/Navigation/NavigationLogo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 
 const NavigationLogo = () => {
   return (
     <div className="col-start-1 flex items-center -mb-6">
       <div className="z-50 mr-4">
-        <a href="/">
+        <Link to="/home">
           <img src={logo} alt="MJ developer Logo" />
-        </a>
+        </Link>
       </div>
     </div>
   );
