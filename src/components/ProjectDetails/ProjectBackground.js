156c70432eef9b4e3a726073867972643dddba7e
Mathius <devachievem@gmail.com>
2021-04-30 11:37:20 -0700

    adjust layout of single project page

--- src/components/ProjectDetails/ProjectBackground.js
+++ src/components/ProjectDetails/ProjectBackground.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ProjectBackground = ({background}) => {
   return (
-    <div className="col-span-2">
-      {background}
+    <div className="col-span-3 space-y-16 mb-16">
+      <h2 className="text-4xl">Project Background</h2>
+      <p className="leading-8">{background}</p>
     </div>
   );
 };
