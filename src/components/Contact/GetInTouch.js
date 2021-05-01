2f320d1932e190ae996554a56c8f51f9e8dd7dec
Mathius <devachievem@gmail.com>
2021-04-30 21:27:36 -0700

    adjust contact layout

---
+++ src/components/Contact/GetInTouch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SocialMediaIcons from '../Footer/SocialMediaIcons';
+
+const GetInTouch = () => {
+  return (
+    <div>
+      <h2 className="text-4xl">Get in Touch</h2>
+      <p>I’d love to hear about what you’re working on and how I could help. I’m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I’m also happy to hear about opportunites that don’t fit that description. I’m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below and get in touch using the form.</p>
+      <SocialMediaIcons color="black" />
+    </div>
+  );
+};
+
+export default GetInTouch;
--- src/components/Footer/SocialMediaIcons.jsx
+++ src/components/Footer/SocialMediaIcons.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const SocialMediaIcons = () => {
+const SocialMediaIcons = ({color}) => {
   return (
-    <div className="space-x-4 md:space-x-1 flex justify-center items-center">
+    <div className={`space-x-4 md:space-x-1 flex justify-center items-center text-${color}`}>
     <a target="_blank" rel="noreferrer" href="https://www.instagram.com/sovereign.birth/"   className="fa-layers fa-fw fa-4x">
-    <FontAwesomeIcon className="text-white" icon={faGithub} transform={'shrink-8'} /> 
+    <FontAwesomeIcon className="" icon={faGithub} transform={'shrink-8'} /> 
     </a>
     <a href="mailto:info@SovereignBirth.ca"   className="fa-layers fa-fw fa-4x">
 
-      <FontAwesomeIcon icon={faTwitter} className="text-white" transform={'shrink-8'} />         
+      <FontAwesomeIcon icon={faTwitter} className="" transform={'shrink-8'} />         
     </a>
     <a target="_blank" rel="noreferrer" href="https://www.facebook.com/SovereignBirth"   className="fa-layers fa-fw ">
-     <FontAwesomeIcon className="text-white fa-4x" icon={faLinkedin} transform={'shrink-8'} />      
+     <FontAwesomeIcon className=" fa-4x" icon={faLinkedin} transform={'shrink-8'} />      
     </a>
   </div>
   );
